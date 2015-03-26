30585f15212fa93ed3b200e8326c12c4fdab432d
craigprofessional <craigveenstra@gmail.com>
2015-03-26 12:26:20 -0400

    Insert Update Delete Sublets

--- model/subletDB.php
+++ model/subletDB.php
@@ -1,40 +1,19 @@
 <?php
 //Craig Veenstra
 
-
-
 require_once 'db_connect.php';
 
-
 ////////////////////////////////////////////////////////////////////////////////////
-/////////       this class gets all sublets from database         //////////////////
-////////////////////////////////////////////////////////////////////////////////////
-
-class Sublets {
-    
-    public static function getSublets(){
-        
-        //connect to the database
-        $db = Db_connect::getDB();
-        
-        //the sql statement in a variable
-        $SQL = 'SELECT * FROM Sublets';
-        
-        //returning the results from the query into $getSublets
-        $getSublets = $db->query($SQL);
-        return $getSublets;    
-    }   
-}
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////
-/////////       this class inserts into the database              //////////////////
+/////////       Interacting with the Sublets table              ////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
 
 class SubletDB {
+    
+    
+    //////////////////////////////////////////
+    /// Inserting into the Sublet Table    ///
+    //////////////////////////////////////////
+    
     public static function insertSublets($subletObject){
         
         //connect to the database
@@ -60,6 +39,68 @@ class SubletDB {
         //putting the values into the database
         $row_count = $stm->execute();
  
+    }
+    
+    
+    
+    //////////////////////////////////////////
+    /// Getting all sublets from the table ///
+    //////////////////////////////////////////
+    
+    public static function getSublets(){
+        
+        //connect to the database
+        $db = Db_connect::getDB();
+        
+        //the sql statement in a variable
+        $SQL = 'SELECT * FROM Sublets';
+        
+        //returning the results from the query into $getSublets
+        $getSublets = $db->query($SQL);
+        return $getSublets;    
+    }   
+    
+    
+    
+    //////////////////////////////////////////
+    /// Deleting sublets from the table //////
+    //////////////////////////////////////////
+    
+    public static function deleteSublet($p_id, $u_id){
+        
+        //connect to the database
+        $db = Db_connect::getDB();
+        
+        //the sql statement in a variable
+        $SQL = 'DELETE FROM Sublets WHERE p_id =' . $p_id . ' AND u_id =' . $u_id;
+        
+        $deleteSublets = $db->query($SQL);
+        return $deleteSublets;   
+    }
+    
+    
+    //////////////////////////////////////////
+    /// Updateing sublets from the table /////
+    //////////////////////////////////////////
+    
+    public static function updateSublet($subletObject){
+        
+        //connect to the database
+        $db = Db_connect::getDB();
+        
+        //un packing the object sent into the static method
+        $u_id = $subletObject->getu_id();
+        $p_id = $subletObject->getp_id();
+        $description = $subletObject->getDescription();
+        $rentAmount = $subletObject->getrentAmount();
+        $startDate = $subletObject->getstartDate();
+        $endDate = $subletObject->getendDate();
+        
+        //the sql statement in a variable
+        $SQL = 'UPDATE Sublets SET info = ' . $description . ', rentAmount = ' . $rentAmount . ', startDate = ' . $startDate . ', endDate = '. $endDate . ' WHERE p_id = ' . $p_id . ' AND u_id = ' . $u_id;
+               
+        $updateSublets = $db->query($SQL);
+        return $updateSublets;
     }
 }
 
