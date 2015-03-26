c7950b8bbf89335a1de572a90ba7dbcd3a6372a9
Sumeet Bhullar <sumeet.bhu@gmail.com>
2015-03-26 15:47:33 -0400

    sublet update link

--- user_dash/user_menu.php
+++ user_dash/user_menu.php
@@ -8,7 +8,6 @@ if($_SESSION['role'] == 'landlord'){
     echo '<li><a href="#">Search for Rents</a></li>';
 }
     echo '<li><a href="'.ROOT.'postratings">Look at ratings</a></li>';
-
 ?>
     </ul>
 </nav>
