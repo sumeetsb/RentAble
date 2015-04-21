ead8274335db142603b078dda6b921373da6ac61
craigprofessional <craigveenstra@gmail.com>
2015-04-21 15:01:24 -0400

    Removed footer / moved FAQ

--- view/header.php
+++ view/header.php
@@ -21,6 +21,7 @@
                 <?php include('adminlog.php'); ?>
             </div>
             <div id="headerlog">
+                <a href="<?php echo ROOT;?>FAQ">FAQ</a>
                 <?php include('loginreg.php'); ?>
             </div>
             <a href="<?php echo ROOT; ?>"><img id="logo" src="<?php echo ROOT;?>images/RentAble_HeaderLogo_197x62px.png" alt="RentAble" height="62" width="197"></a>
