5b2dd5e085b30bfc5b9010105ec0f13541dc1ab0
Sumeet Bhullar <sumeet.bhu@gmail.com>
2015-04-22 14:54:50 -0400

    pay invoice link for tenants

--- user_dash/user_menu.php
+++ user_dash/user_menu.php
@@ -6,6 +6,7 @@ if($_SESSION['role'] == 'landlord'){
     echo '<li><a href="'.ROOT.'Payment/PaymentView/CreateInvoice.php">Create Invoice for tenant</a></li>';
     echo '<li><a href="'.ROOT.'register_property">Register a Property</a></li>';
 } else {
+    echo '<li><a href="'.ROOT.'Payment/PaymentView/MyInvoices.php">Pay Rent</a></li>';
     echo '<li><a href="#">Search for Rents</a></li>';
 }
     echo '<li><a href="'.ROOT.'postratings">Look at ratings</a></li>';
