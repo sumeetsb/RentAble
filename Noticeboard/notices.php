43ffd1452248249ac4ef1b293d16ebbe5281763b
Sumeet Bhullar <sumeet.bhu@gmail.com>
2015-04-22 17:55:09 -0400

    changes and fixes

--- Noticeboard/notices.php
+++ Noticeboard/notices.php
@@ -5,8 +5,17 @@ include '../view/header.php';
 
 <h2><?php echo $pname; ?> Notice Board</h2>
 
+<table class="notices">
+<?php
+    foreach($notices as $notice){
+        
+?>
+    
 
-
+<?php
+    }
+?>
+</table>
 
 <?php 
 
