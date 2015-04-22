468095fb5776ed4bf1bf247f9a4d99af8a67463c
Sumeet Bhullar <sumeet.bhu@gmail.com>
2015-04-22 14:46:01 -0400

    create invoice link

--- user_dash/user_menu.php
+++ user_dash/user_menu.php
@@ -3,6 +3,7 @@
     <ul>
 <?php
 if($_SESSION['role'] == 'landlord'){
+    echo '<li><a href="'.ROOT.'Payment/PaymentView/CreateInvoice.php">Create Invoice for tenant</a></li>';
     echo '<li><a href="'.ROOT.'register_property">Register a Property</a></li>';
 } else {
     echo '<li><a href="#">Search for Rents</a></li>';
